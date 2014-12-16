c02aea1ec3ea54113f7db9405a7843dfa18453a8
unknown <invazorcool@hotmail.com>
2014-12-15 16:44:00 -0800

    Prueba 2

--- starrett1.php
+++ starrett1.php
@@ -161,14 +161,26 @@ $response = $client->get($raiz_fuente . $edp);
 $regex = '/<div class=\"image\"(.*?)<\/div>/s';
 $regex1= '/a href=\"(.*?)\"/';
 
+//new
+$csvIterator = new CsvIterator('starrett0.csv');
+foreach ($csvIterator as $row => $data) {
+    $edp = $data[1];
+    break;
+
+    }
+
+
+
 if ( preg_match_all($regex, $response, $list) )
     {
     	if ( preg_match($regex1, $list[0][0], $list1) )
-    		{ /*echo $i;*/ $a1=$list1[1];}
+    		{ /*echo $i;*/ $a1=$list1[1];
+                copyRemote($a1, $miruta . $edp . ".jpg");
+            }
 		else
     		{ print "Not found 1"; $a1="";}
     	//echo '<img src="' . $a1 . '">';
-    	copyRemote($a1, $miruta . $edp . ".jpg");
+    	
     }
     else
     	print "Not found 0";
